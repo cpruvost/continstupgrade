6f8f9e67260540b030e40e05414a493b85cb97ad
Christophe Pruvost <christophe.pruvost@oracle.com>
2023-10-31 09:49:51 +0100

    update module apigateway

--- modules/apigateway/output.tf
+++ modules/apigateway/output.tf
@@ -1,7 +1,9 @@
 output "gateways" {
-  value = data.oci_apigateway_gateways.test_gateways
+  #value = data.oci_apigateway_gateways.test_gateways
+  value = oci_apigateway_gateway.test_gateway
 }
 
 output "deployments" {
-  value = data.oci_apigateway_deployments.test_deployments
+  #value = data.oci_apigateway_deployments.test_deployments
+  value = oci_apigateway_deployment.test_deployment
 }
