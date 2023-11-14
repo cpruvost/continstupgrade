a5e724593b863e6c2c7d15fc9640b5f14fdc0385
Christophe Pruvost <christophe.pruvost@oracle.com>
2023-11-14 15:14:14 +0100

    update Docker Hub

--- modules/containerinstance/main.tf
+++ modules/containerinstance/main.tf
@@ -150,7 +150,8 @@ resource "oci_container_instances_container_instance" "this" {
 
   image_pull_secrets {
         #Required
-        registry_endpoint = "fra.ocir.io"
+        #registry_endpoint = "fra.ocir.io"
+        registry_endpoint = "docker.io"
         #secret_type = "BASIC"
         #username = base64encode("username")
         #password = base64encode("password")
